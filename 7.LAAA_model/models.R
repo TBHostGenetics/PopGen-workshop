67438b881ad53f3beaa2319be4ef53922b3c99bb
DaynaSmith <104357839+DaynaSmith@users.noreply.github.com>
2025-02-27 21:29:57 +0200

    Update models.R

--- 7.LAAA_model/models.R
+++ 7.LAAA_model/models.R
@@ -1,17 +1,17 @@
 runLaaaModelSummary <- function(model.frame) {
-  return (summary(glm(Class ~  Age + Gender + SanAncestry + AfricanAncestry + EuropeanAncestry + SouthEastAsianAncestry  + allele_dose + san_dose + allele_san_dose, data=model.frame, family = "binomial")))
+  return (summary(glm(Class ~  Age + Sex + AFR + SAN + allele_dose + bantu_dose + allele_bantu_dose, data=model.frame, family = "binomial")))
 }
 
 runNullModel <- function(model.frame) {
-  return (glm(Class ~ Age + Gender + SanAncestry + AfricanAncestry + EuropeanAncestry + SouthEastAsianAncestry , data=model.frame, family = "binomial"))
+  return (glm(Class ~ Age + Sex + AFR + SAN, data=model.frame, family = "binomial"))
 }
 
 runLaaaModel <- function(model.frame) {
-  return (glm(Class ~ Age + Gender + SanAncestry + AfricanAncestry + EuropeanAncestry + SouthEastAsianAncestry  + allele_dose + san_dose + allele_san_dose, data=model.frame, family = "binomial"))
+  return (glm(Class ~ Age + Sex + SAN + AFR + allele_dose + bantu_dose + allele_bantu_dose, data=model.frame, family = "binomial"))
 }
 
 runAlleleModelSummary <- function(model.frame) {
-  return (summary(glm(Class ~ Age + Gender + SanAncestry + AfricanAncestry + EuropeanAncestry + SouthEastAsianAncestry  + allele_dose, data=model.frame, family = "binomial")))
+  return (summary(glm(Class ~ Age + Sex + SAN + AFR + allele_dose, data=model.frame, family = "binomial")))
 }
 
 
