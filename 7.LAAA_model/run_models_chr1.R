8e972612a502b6c9d8baca0f49c4e6ac695aa782
DaynaSmith <104357839+DaynaSmith@users.noreply.github.com>
2025-02-27 21:20:30 +0200

    Update run_models_chr1.R

--- 7.LAAA_model/run_models_chr1.R
+++ 7.LAAA_model/run_models_chr1.R
@@ -3,7 +3,7 @@ out.file.name <- "/mnt/lustre/users/yswart/Dosages_popgen/BANTU/LAAA_bantu_chr1.
 pheno.frame <- read.delim("/mnt/lustre/users/yswart/Dosages_popgen/Phenotype.txt", stringsAsFactors = F)
 allele.frame <- read.delim("/mnt/lustre/users/yswart/Dosages_popgen/BANTU/chr1_allele_dose.txt", stringsAsFactors = F)
 bantu.frame <- read.delim("/mnt/lustre/users/yswart/Dosages_popgen/BANTU/chr1_bantu_dose.txt", stringsAsFactors = F)
-allele.san.frame <- read.delim("/mnt/lustre/users/yswart/Dosages_popgen/BANTU/chr1_allele_bantu_dose.txt", stringsAsFactors = F)
+allele.bantu.frame <- read.delim("/mnt/lustre/users/yswart/Dosages_popgen/BANTU/chr1_allele_bantu_dose.txt", stringsAsFactors = F)
 cat("position\tref\talt\talt_frq\tunadj_allele_dose_beta\tunadj_allele_dose_beta_se\tunadj_allele_dose_p\tallele_dose_beta\tallele_dose_beta_se\tallele_dose_p\tbantu_dose_beta\tbantu_dose_beta_se\tbantu_dose_p\tallele_bantu_dose_beta\tallele_bantu_dose_beta_se\tallele_bantu_dose_p\tanova_p\n", sep="", file=out.file.name, append=F)
 
 for (position in allele.frame$position) {
@@ -14,19 +14,19 @@ for (position in allele.frame$position) {
   names(allele.col.frame)[2] <- "allele_dose"
   model.frame <- merge(pheno.frame, allele.col.frame)
   
-  #Merge in the san dose
-  san.col.frame <- 
-    data.frame(sample_id=names(san.frame)[-c(1:3)],
-               san_dose=t(san.frame[san.frame$position == position,-c(1:3)]))
-  names(san.col.frame)[2] <- "san_dose"
-  model.frame <- merge(model.frame, san.col.frame)
+  #Merge in the bantu dose
+  bantu.col.frame <- 
+    data.frame(sample_id=names(bantu.frame)[-c(1:3)],
+               bantu_dose=t(bantu.frame[bantu.frame$position == position,-c(1:3)]))
+  names(bantu.col.frame)[2] <- "bantu_dose"
+  model.frame <- merge(model.frame, bantu.col.frame)
   
-  #Merge in the allele.san dose
-  allele.san.col.frame <- 
-    data.frame(sample_id=names(allele.san.frame)[-c(1:3)],
-               allele.san_dose=t(allele.san.frame[allele.san.frame$position == position,-c(1:3)]))
-  names(allele.san.col.frame)[2] <- "allele_san_dose"
-  model.frame <- merge(model.frame, allele.san.col.frame)
+  #Merge in the allele.bantu dose
+  allele.bantu.col.frame <- 
+    data.frame(sample_id=names(allele.bantu.frame)[-c(1:3)],
+               allele.bantu_dose=t(allele.bantu.frame[allele.bantu.frame$position == position,-c(1:3)]))
+  names(allele.bantu.col.frame)[2] <- "allele_bantu_dose"
+  model.frame <- merge(model.frame, allele.bantu.col.frame)
   
   #Fit the model
   model <- runLaaaModelSummary(model.frame)
@@ -52,23 +52,23 @@ for (position in allele.frame$position) {
     allele_dose_beta_se <- NA
     allele_dose_p <- NA  
   }
-  if ("san_dose" %in% rownames(model$coefficients)) {
-    san_dose_beta <- model$coefficients["san_dose", "Estimate"]
-    san_dose_beta_se <- model$coefficients["san_dose", "Std. Error"]
-    san_dose_p <- model$coefficients["san_dose", "Pr(>|z|)"]      
+  if ("bantu_dose" %in% rownames(model$coefficients)) {
+    bantu_dose_beta <- model$coefficients["bantu_dose", "Estimate"]
+    bantu_dose_beta_se <- model$coefficients["bantu_dose", "Std. Error"]
+    bantu_dose_p <- model$coefficients["bantu_dose", "Pr(>|z|)"]      
   } else {
-    san_dose_beta <- NA
-    san_dose_beta_se <- NA
-    san_dose_p <- NA  
+    bantu_dose_beta <- NA
+    bantu_dose_beta_se <- NA
+    bantu_dose_p <- NA  
   }
-  if ("allele_san_dose" %in% rownames(model$coefficients)) {
-    allele_san_dose_beta <- model$coefficients["allele_san_dose", "Estimate"]
-    allele_san_dose_beta_se <- model$coefficients["allele_san_dose", "Std. Error"]
-    allele_san_dose_p <- model$coefficients["allele_san_dose", "Pr(>|z|)"]      
+  if ("allele_bantu_dose" %in% rownames(model$coefficients)) {
+    allele_bantu_dose_beta <- model$coefficients["allele_bantu_dose", "Estimate"]
+    allele_bantu_dose_beta_se <- model$coefficients["allele_bantu_dose", "Std. Error"]
+    allele_bantu_dose_p <- model$coefficients["allele_bantu_dose", "Pr(>|z|)"]      
   } else {
-    allele_san_dose_beta <- NA
-    allele_san_dose_beta_se <- NA
-    allele_san_dose_p <- NA  
+    allele_bantu_dose_beta <- NA
+    allele_bantu_dose_beta_se <- NA
+    allele_bantu_dose_p <- NA  
   }
   if ("allele_dose" %in% rownames(allele.model$coefficients)) {
     unadj_allele_dose_beta <- allele.model$coefficients["allele_dose", "Estimate"]
@@ -88,12 +88,12 @@ for (position in allele.frame$position) {
       allele_dose_beta,"\t",
       allele_dose_beta_se,"\t",
       allele_dose_p,"\t",
-      san_dose_beta,"\t",
-      san_dose_beta_se,"\t",
-      san_dose_p,"\t",
-      allele_san_dose_beta,"\t",
-      allele_san_dose_beta_se,"\t",
-      allele_san_dose_p, "\t",
+      bantu_dose_beta,"\t",
+      bantu_dose_beta_se,"\t",
+      bantu_dose_p,"\t",
+      allele_bantu_dose_beta,"\t",
+      allele_bantu_dose_beta_se,"\t",
+      allele_bantu_dose_p, "\t",
       anova_p, "\n", file=out.file.name, append=T, sep="")
   
 }
